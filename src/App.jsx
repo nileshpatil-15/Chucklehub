fd9a2cae6e19745b0020fd3c166b19eda920d633
Nilesh <nileshpatil1111-5@gmail.com>
2024-03-13 23:51:50 +0530

    set basic lauout and form validation

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,27 @@
-import { useState } from 'react'
-import './App.css'
-import { Button } from 'react-bootstrap';
+import "./App.css";
+import {
 
+  Route,
+  Routes,
+  
+} from "react-router-dom";
+import AuthLayout from "./auth/AuthLayout";
+import LoginForm from "./auth/form/LoginForm";
+import Home from "./root/pages/Home";
+import RootLayout from "./root/RootLayout";
 function App() {
-
   return (
-    <>
-       <Button>
-         
-        </Button>
-    </>
-  )
+    <main className=" d-flex vh-100">
+      <Routes>
+        <Route element={<AuthLayout />}>
+          <Route index  element={<LoginForm/>}/>
+        </Route>
+        <Route element={<RootLayout />}>
+          <Route path={"/home"}  element={<Home />} />
+        </Route>
+      </Routes>
+    </main>
+  );
 }
 
-export default App
+export default App;
